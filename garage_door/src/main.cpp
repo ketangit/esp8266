5be8f4adc569004c613b097e2b500c3e15166462
Ketan Patel <ketan.patel@live.com>
2019-07-14 22:34:57 -0400

    updated mqtt and neo pixel library

--- garage_door/src/main.cpp
+++ garage_door/src/main.cpp
@@ -1,16 +1,19 @@
 #include <Arduino.h>
-#include <Ticker.h>
 #include <ESP8266WiFi.h>
-#include <AsyncMqttClient.h>
-#include <NeoPixelBus.h>
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_MQTT_Client.h>
 #include <Debounce.h> // https://github.com/arnebech/Debounce (copied in lib folder)
 #include <Timer.h>    // https://github.com/JChristensen/Timer/tree/v2.1 (copied in lib folder)
 
-#define WIFI_SSID "ENTER_SSID"            // "ENTER_SSID"
-#define WIFI_PASS "ENTER_SSID_PWD"        // "ENTER_SSID_PWD"
-#define MQTT_HOST IPAddress(127, 0, 0, 1) // "ENTER_MQTT_IP"
-#define MQTT_PORT 1883
+#define WIFI_SSID "" // "ENTER_SSID"
+#define WIFI_PASS "" // "ENTER_SSID_PWD"
 
+String deviceName = "GARAGE-1";
+const char MQTT_SERVER[] PROGMEM = "";   // "ENTER_MQTT_IP";
+const char MQTT_USERNAME[] PROGMEM = ""; // "ENTER_MQTT_USR";
+const char MQTT_PASSWORD[] PROGMEM = ""; // "ENTER_MQTT_PWD";
+
+#define MQTT_PORT 1883
 #define GRGE_1_PIN 12 // Garage Door - 1 input pin
 #define GRGE_2_PIN 14 // Garage Door - 2 input pin
 #define GRGE_3_PIN 13 // Garage Door - 3 input pin
@@ -25,38 +28,25 @@
 #define GRGE_LED_1 2           // Garage Door - 1 LED
 #define GDOOR_OPEN_TIME 300000 // 5 minutes
 
+#define PIN 2 // WS Led pin
+#define NUMPIXELS 3
+
 char szBuffer[100] = "\0";
 char szState[30] = "\0";
-long lastReconnectAttempt = 0;
 int8_t doorOpenTimerArray[3] = {0};
-long doorOpenTotalTime[3] = {0};
-String deviceName = "GARAGE-1";
+int doorOpenTotalTime[3] = {0};
 
 Debounce debounce = Debounce();
 Timer timer;
-WiFiEventHandler wifiConnectHandler;
-WiFiEventHandler wifiDisconnectHandler;
-Ticker wifiReconnectTimer;
-AsyncMqttClient mqttClient;
-Ticker mqttReconnectTimer;
-
-#define colorSaturation 128
-const uint16_t PixelCount = 3; // number of NeoPixels (one for each garage door)
-const uint8_t PixelPin = 2;    // ignored for Esp8266 default GPIO2
-NeoPixelBus<NeoGrbFeature, NeoEsp8266Uart1800KbpsMethod> strip(PixelCount, PixelPin);
-RgbColor red(colorSaturation, 0, 0);
-RgbColor green(0, colorSaturation, 0);
+WiFiClient client;
+Adafruit_MQTT_Client mqttClient(&client, MQTT_SERVER, MQTT_PORT, MQTT_USERNAME, MQTT_PASSWORD);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void pingMQTTMessage(void *context);
 void sendMessage(String topic, String message);
 void callbackGarage(bool state, uint8_t pin);
 void callbackDoorOpen(void *context);
-void connectToWifi();
-void onWifiConnect(const WiFiEventStationModeGotIP &event);
-void onWifiDisconnect(const WiFiEventStationModeDisconnected &event);
-void connectToMqtt();
-void onMqttConnect(bool sessionPresent);
-void onMqttDisconnect(AsyncMqttClientDisconnectReason reason);
+void mqttConnect();
 
 void setup()
 {
@@ -67,20 +57,23 @@ void setup()
   //Serial.flush();
 
   pinMode(STATUS_LED, OUTPUT);
+  pinMode(GRGE_1_PIN, INPUT_PULLUP);
+  pinMode(GRGE_2_PIN, INPUT_PULLUP);
+  pinMode(GRGE_3_PIN, INPUT_PULLUP);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+  }
+
   timer.oscillate(STATUS_LED, LED_FLASH_TIME, HIGH);
   timer.every(MQTT_PING_TIME, pingMQTTMessage, (void *)0);
 
-  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
-  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
-
-  mqttClient.onConnect(onMqttConnect);
-  mqttClient.onDisconnect(onMqttDisconnect);
-  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
-
-  connectToWifi();
-
-  strip.Begin();
-  strip.Show();
+  strip.begin();
+  strip.setPixelColor(0, strip.Color(0, 0, 0));
+  strip.setPixelColor(1, strip.Color(0, 0, 0));
+  strip.setPixelColor(2, strip.Color(0, 0, 0));
 
   callbackGarage(digitalRead(GRGE_1_PIN), GRGE_1_PIN);
   callbackGarage(digitalRead(GRGE_2_PIN), GRGE_2_PIN);
@@ -94,8 +87,10 @@ void setup()
 
 void loop()
 {
+  mqttConnect();
   debounce.update();
   timer.update();
+  strip.show();
 }
 
 void pingMQTTMessage(void *context)
@@ -106,7 +101,7 @@ void pingMQTTMessage(void *context)
 
 void sendMessage(String topic, String message)
 {
-  mqttClient.publish((char *)topic.c_str(), 1, true, (char *)message.c_str());
+  mqttClient.publish((char *)topic.c_str(), (char *)message.c_str());
 }
 
 void callbackGarage(bool state, uint8_t pin)
@@ -130,16 +125,16 @@ void callbackGarage(bool state, uint8_t pin)
   }
   if (state)
   {
-    strip.SetPixelColor(ledNum, red);
+    strip.setPixelColor(ledNum, strip.Color(150, 0, 0));
     doorOpenTimerArray[ledNum] = timer.every(GDOOR_OPEN_TIME, callbackDoorOpen, (void *)pin);
   }
   else
   {
-    strip.SetPixelColor(ledNum, green);
+    strip.setPixelColor(ledNum, strip.Color(0, 150, 0));
     doorOpenTotalTime[ledNum] = 0;
     timer.stop(doorOpenTimerArray[ledNum]);
   }
-  strip.Show();
+  strip.show();
   sprintf(szBuffer, "SENSOR/%s/PORT/%u", deviceName.c_str(), doorNum);
   sprintf(szState, "%s", (state ? "Open" : "Close"));
   sendMessage(szBuffer, szState);
@@ -171,40 +166,16 @@ void callbackDoorOpen(void *context)
   sendMessage(szBuffer, szState);
 }
 
-/*
-  Wifi and MQTT functions
- */
-void connectToWifi()
+void mqttConnect()
 {
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-}
-
-void onWifiConnect(const WiFiEventStationModeGotIP &event)
-{
-  connectToMqtt();
-}
-
-void onWifiDisconnect(const WiFiEventStationModeDisconnected &event)
-{
-  mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
-  wifiReconnectTimer.once(2, connectToWifi);
-}
-
-void connectToMqtt()
-{
-  mqttClient.connect();
-}
-
-void onMqttConnect(bool sessionPresent)
-{
-  sprintf(szBuffer, "SENSOR/%s/STATUS", deviceName.c_str());
-  sendMessage(szBuffer, "STARTED");
-}
-
-void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
-{
-  if (WiFi.isConnected())
+  int8_t ret;
+  if (mqttClient.connected())
   {
-    mqttReconnectTimer.once(2, connectToMqtt);
+    return;
+  }
+  while ((ret = mqttClient.connect()) != 0)
+  { // connect will return 0 for connected
+    mqttClient.disconnect();
+    delay(5000); // wait 5 seconds
   }
 }
